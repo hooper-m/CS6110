befe3d261026726e3a151037f8c3de48b2d416d2
mhooper <hooperm.161@gmail.com>
2020-02-28 13:11:22 -0700

    Added sample analyzers

---
+++ ExclusiveIfs/ExclusiveIfs/IfInsteadOfWhileLoopAnalyzer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace IfInsteadOfWhileLoop {
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class IfInsteadOfWhileLoopAnalyzer : DiagnosticAnalyzer {
+        public const string DiagnosticId = "IfInsteadOfWhileLoop";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Novice Style";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context) {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.WhileStatement);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context) {
+            var whileClause = (WhileStatementSyntax)context.Node;
+
+            if (whileClause.Statement is ReturnStatementSyntax returnStatement) {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+            }
+
+            else if (whileClause.Statement is BlockSyntax loopBody) {
+                if (loopBody.Statements.Last() is ReturnStatementSyntax bodyReturnSatement) {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+                }
+            }
+        }
+    }
+}
